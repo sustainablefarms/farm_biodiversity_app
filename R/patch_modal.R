d7a3c5ea47b818dd59192d577b09f5193868adc0
Kassel Hingee <kassel.hingee@gmail.com>
2020-09-01 20:28:58 +1000

    popify/tipify of midstory info almost works - text is just too long

--- R/patch_modal.R
+++ R/patch_modal.R
@@ -17,11 +17,14 @@ patch_modal <- function(
         "The amount of woody vegetation canopy within 500m of the patch centre, as a percentage of the area within 500m."
         # "The percentage of area within 500m of the patch centre covered by woody vegetation canopy."
         ),
-      sliderInput(
+      popify(sliderInput(
         inputId = paste0("pc_midstorey_", value),
-        label = "Amount of midstorey cover (%)",
+        label = "Vegetation between 2m and 10m high (% area of patch)",
         min = 0, max = 10, step = 1,
         value = midstorey),
+        title = "",
+        content = "The percentage of the patch that has vegetation between 2m and 10m tall (e.g regrowth eucalypt or acacia). Here are examples of what this looks like (insert insert...).<br> If you're interested in greater robustness, our ecologists estimated this quantity using 2x 50m point-intersect transects.",
+        options = list(html = "true")),
       checkboxInput(
         inputId = paste0("noisy_miner_", value),
         label = "Noisy Miners present?",
