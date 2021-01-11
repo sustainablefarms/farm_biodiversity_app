d421a5c30b833fda05476d66537493fbfd8f6e0e
Kassel Hingee <kassel.hingee@gmail.com>
2021-01-11 11:27:33 +1100

    test that runs all prediction plots works

---
+++ tests/testthat/test-all_plots.R
@@ -0,0 +1,33 @@
+# test that all plots can be built
+test_that("plots for Gundagai", {
+  model_data <- load_model_data()
+  new_data_mean <- get_new_data_mean(model_data)
+  selected_region <- "Gundagai"
+  current_values <- list(AnnMeanTemp = NULL,
+                         AnnPrec = 704,
+                         AnnTempRange = NULL,
+                         MaxTWarmMonth = NULL,
+                         MinTColdMonth  = NULL,
+                         PrecSeasonality = 15,
+                         PrecWarmQ = NULL,
+                         latitude = NULL,
+                         midstorey = 6,
+                         noisy_miner = TRUE,
+                         patches = 1,
+                         woody_veg = 8,
+                         year = 2018)
+  points <- readRDS("data/sa2_points_climate.rds")
+  preddata <- compute_prediction_data(model_data, current_values, new_data_mean,
+                                      points, selected_region)
+  richness_plot(preddata$species_richness)
+  species_ggplot(
+    df = preddata$species_predictions$common,
+    title = "Most likely species",
+    add_plus = FALSE,
+    errorbar = TRUE)
+  species_ggplot(
+    df = preddata$species_predictions$different,
+    title = "Locally prevalent species",
+    add_plus = TRUE)
+})
+
