92c6847034a423f272f5f22da3820ad428a21b29
Kassel Hingee <kassel.hingee@gmail.com>
2021-02-05 17:09:50 +1100

    testing of species plot updated for new inputs from current_values

--- tests/testthat/test-all_plots.R
+++ tests/testthat/test-all_plots.R
@@ -5,22 +5,8 @@ test_that("prediction plots for Gundagai", {
   model_data <- load_model_data()
   new_data_mean <- get_new_data_mean(model_data)
   selected_region <- "Gundagai"
-  current_values <- list(AnnMeanTemp = NULL,
-                         AnnPrec = 704,
-                         AnnTempRange = NULL,
-                         MaxTWarmMonth = NULL,
-                         MinTColdMonth  = NULL,
-                         PrecSeasonality = 15,
-                         PrecWarmQ = NULL,
-                         latitude = NULL,
-                         midstorey = 6,
-                         noisy_miner = TRUE,
-                         patches = 1,
-                         woody_veg = 8,
-                         year = 2018)
-  points <- readRDS("data/sa2_points_climate.rds")
-  preddata <- compute_prediction_data(model_data, current_values, new_data_mean,
-                                      points, selected_region)
+  current_values <- isolate(reactiveValuesToList(readRDS("./current_values_two_patches.rds")))
+  preddata <- compute_prediction_data(model_data, current_values, new_data_mean)
   richness_plot(preddata$species_richness)
   species_ggplot(
     df = preddata$species_predictions$common,
