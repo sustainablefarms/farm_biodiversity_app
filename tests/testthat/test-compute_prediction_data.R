02e107e6f25630bc9736d93acbbcc2a6f61fec11
Kassel Hingee <kassel.hingee@gmail.com>
2021-02-09 11:18:52 +1100

    computing prediction data tested more

--- tests/testthat/test-compute_prediction_data.R
+++ tests/testthat/test-compute_prediction_data.R
@@ -2,20 +2,7 @@ test_that("compute prediction data runs on Gundagai", {
   model_data <- load_model_data()
   new_data_mean <- get_new_data_mean(model_data)
   selected_region <- "Gundagai"
-  current_values <- list(AnnMeanTemp = NULL,
-                         AnnPrec = 704,
-                         AnnTempRange = NULL,
-                         MaxTWarmMonth = NULL,
-                         MinTColdMonth  = NULL,
-                         PrecSeasonality = 15,
-                         PrecWarmQ = NULL,
-                         latitude = NULL,
-                         midstorey = 6,
-                         noisy_miner = TRUE,
-                         patches = 1,
-                         woody_veg = 8,
-                         year = 2018)
+  current_values <- isolate(reactiveValuesToList(readRDS("./current_values_two_patches.rds")))
   points <- readRDS("data/sa2_points_climate.rds")
-  preddata <- compute_prediction_data(model_data, current_values, new_data_mean,
-                          points, selected_region)
+  preddata <- compute_prediction_data(model_data, current_values, new_data_mean)
 })
