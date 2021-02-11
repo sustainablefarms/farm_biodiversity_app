1655dbf46d7df2deb9538a0dbfa4ee8b8d76ac47
Kassel Hingee <kassel.hingee@gmail.com>
2021-02-11 15:27:15 +1100

    worked out how to use add_trace and style to build up a plot

--- plottrials/trial_plotly.R
+++ plottrials/trial_plotly.R
@@ -8,31 +8,30 @@ df <- topnrows(tocommon(species_prob_current), 10, "value")
 df$label <- paste0("", round(df$value * 100, 0), "%")
 
 df$tooltip <- paste0(df$species, " has some interest features.")
-plt <- plot_ly(data = df,
-                 y = ~species,
-                 x = ~value,
-                 color = ~value,
-                 marker = list(colorscale = "Blues", #"[[0, '#006666ff'], [1, '#178BCAff']]", 
-                               reversescale = TRUE, cmax = 1.2, cmin = 0.3), #cmin = 0, cmax = 1),
-                 # text = paste0(round(data$value, digits = 2) * 100, "%"),
-                 text = df$tooltip,
-                 # hoverinfo = 'text',
-                 # textposition = 'auto',
-                 hovertemplate = paste('<b>%{text}</b><extra></extra>'), #the <extra></extra> removes the 'trace 0' extra information
-                 error_x = ~list(type = 'data',
-                                 array = upper - value,
-                                 arrayminus = value - lower,
-                                 symmetric = FALSE,
-                                 color = '#000000'),
-                 type = "bar") %>%
-    add_annotations(x  = ~lower, 
-                    y = ~species, 
-                    text = df$label,
-                    xanchor = "right",
-                    xshift = -3,
-                    bgcolor = "rgba(255,255,255,1)",
-                    showarrow = FALSE)
-plt <- plt %>%
+plot_ly(data = df) %>% #initiate plot
+  add_trace(type = "bar",  #make a bar plot
+            y = ~species,
+            x = ~value,
+            color = ~value,
+            marker = list(colorscale = "Blues", #"[[0, '#006666ff'], [1, '#178BCAff']]", 
+                          reversescale = TRUE, cmax = 1.2, cmin = 0.3) #cmin = 0, cmax = 1)
+            ) %>%
+  # add error bars
+  style(error_x = list(visible = TRUE,
+                        type = 'data',
+                            array = df$upper - df$value,
+                            arrayminus = df$value - df$lower,
+                            symmetric = FALSE,
+                            color = '#000000')) %>%
+  # add the values onto the bars
+  add_annotations(x  = ~lower, 
+                  y = ~species, 
+                  text = df$label,
+                  xanchor = "right",
+                  xshift = -3,
+                  bgcolor = "rgba(255,255,255,1)",
+                  showarrow = FALSE) %>%
+  # add the species names
   add_annotations(x  = 0, 
                   y = ~species, 
                   text = ~species,
@@ -40,9 +39,11 @@ plt <- plt %>%
                   xshift = 3,
                   font = list(color = "rgba(255,255,255,1)"),
                   showarrow = FALSE) %>%
+  # alter layout
   layout(yaxis = ~list(categoryorder = "array", categoryarray = value, autorange = "reversed", visible = FALSE)) %>%
   layout(xaxis = list(visible = FALSE),
          margin = list(l = 0, r = 0, t = 0, b = 0)) %>%
   hide_colorbar() %>%
   config(displayModeBar = FALSE)
-plt
+
+
