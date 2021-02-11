0114c0fb214899884c22c5a4f51e0f7565256f04
Kassel Hingee <kassel.hingee@gmail.com>
2021-02-11 11:18:46 +1100

    test predictions module on multiple patches

--- tests/testthat/test-predictions_module.R
+++ tests/testthat/test-predictions_module.R
@@ -9,3 +9,14 @@ shinyApp(predictionsUI("pred"),
            predictionsServer("pred", reactive(selected_region), current_values,
                              model_data, new_data_mean)
            })
+
+devtools::load_all()
+model_data <- load_model_data()
+new_data_mean <- get_new_data_mean(model_data)
+current_values <-  readRDS("current_values_two_patches.rds")
+current_values$AnnPrec <- NULL
+shinyApp(predictionsUI("pred"),
+         function(input, output, session){
+           predictionsServer("pred", reactive(selected_region), current_values,
+                             model_data, new_data_mean)
+         })
