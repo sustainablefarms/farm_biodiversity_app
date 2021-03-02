89203cbb03c23c0c9b0c3877b8e59197735cc10a
Kassel Hingee <kassel.hingee@gmail.com>
2021-03-02 11:38:04 +1100

    by moving main UI and server to R/, have stopped the warning about a subdirectory, but more work still needed to shift things over fully

---
+++ R/overarchingUIserver.R
@@ -0,0 +1,99 @@
+# overarching UI and Server
+myapp <- function(){
+# UI
+  ui <- fluidPage(
+    includeCSS("./www/base.css"),
+    # fluidRow(HTML("
+    #   <div class='header'>
+    #     <img class='logo' src='SF Logo Vertical - Transparent Background.png' alt='SF logo'>
+    #     <span class='main'>DRAFT Woodland Remnant Bird Biodiversity Estimator</span>
+    #     <span class='subtitle'><br>Version 0.1. By Martin Westgate & Kassel Hingee</span>
+    #   </div>
+    # ")),
+    HTML("<div class='header'>"),
+    fluidRow(
+      column(width = 2,
+      HTML("
+        <img class='logo' src='SF Logo Vertical - Transparent Background.png' alt='SF logo'>
+    ")),
+    column(width = 10, offset = 0, HTML("
+      <span class='main'>Woodland Remnant Bird Biodiversity Estimator</span>
+      <span class='subtitle'><br>Version 0.2 (DRAFT). By Martin Westgate & Kassel Hingee</span>
+    "))),
+    HTML("</div>"),
+    column(width = 1),
+    column(width = 3,
+      selectlocationUI("location"),
+      # plotOutput("climate", height = "300px")
+    ),
+    column(width = 1),
+    column(width = 6,
+      fluidRow(
+        selectpatchUI("patch")
+      ),
+      fluidRow(
+        predictionsUI("pred")
+      )
+    ),
+    title = "SF Model Visualiser",
+    theme = bslib::bs_theme(version = 3, "lumen")
+    # theme = shinytheme("lumen")
+  )
+
+
+# SERVER
+server <- function(input, output, session) {
+
+  # set up required data
+  # 1. from model
+  # 2. reactive values
+  data <- reactiveValues(
+    selected_region = NULL,
+    species_predictions = NULL)
+  current_values <- reactiveValues(
+    patches = 1,
+    woody_veg = NA,
+    midstorey = NA,
+    noisy_miner = NA,
+    year = 2018,
+    AnnPrec = NULL,
+    MaxTWarmMonth = NULL,
+    MinTColdMonth = NULL,
+    PrecSeasonality = NULL,
+    latitude = NULL,
+    AnnPrec = NULL,
+    AnnTempRange = NULL,
+    PrecSeasonality = NULL,
+    PrecWarmQ = NULL)
+
+  ## PATCH (and year)
+  frompatch <- selectpatchServer("patch")
+  observe({
+    current_values$patches <- frompatch$patches
+    current_values$woody_veg <- frompatch$woody_veg
+    current_values$midstorey <- frompatch$midstorey
+    current_values$noisy_miner <- frompatch$noisy_miner
+    current_values$year <- frompatch$year
+  })
+
+  ## REGION
+  outOfModule <- selectlocationServer("location")
+  observe({
+    data$selected_region <- outOfModule$selected_region
+    current_values$AnnPrec <- outOfModule$AnnPrec
+    current_values$MaxTWarmMonth                   <- outOfModule$MaxTWarmMonth
+    current_values$MinTColdMonth                   <- outOfModule$MinTColdMonth
+    current_values$PrecSeasonality                   <- outOfModule$PrecSeasonality
+    current_values$latitude  <- outOfModule$latitude
+  })
+
+  ## PREDICTIONS
+  predictionsServer("pred", current_values,
+                    model_data, new_data_mean,
+                    report_path)
+
+} # end server
+
+
+shinyApp(ui, server)
+}
