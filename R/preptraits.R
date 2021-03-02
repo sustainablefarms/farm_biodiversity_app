54c8b01e8268fe0e08f8e0c501accc746f2a9925
Kassel Hingee <kassel.hingee@gmail.com>
2021-03-02 11:20:33 +1100

    traits saved to small compressed file

---
+++ R/preptraits.R
@@ -0,0 +1,20 @@
+# prep and load traits
+
+preptraits <- function(model_data){
+  traits <- read.csv("../sflddata/private/data/raw/Australian_Bird_Data_Version_1.csv", 
+           stringsAsFactors = FALSE) %>%
+    dplyr::filter(X3_Taxon_common_name_2 %in% model_data$species) %>%
+    dplyr::select(`Common Name` = X3_Taxon_common_name_2,
+                  `Scientific Name` = X7_Taxon_scientific_name_CandB_2, 
+                  `Body Length` = X96_Body_length_8,
+                  `Body Mass` = X99_Body_mass_average_8) %>%
+    dplyr::mutate(`Body Length` = as.numeric(`Body Length`),
+                  `Body Mass` = as.numeric(`Body Mass`))
+  saveRDS(traits, file = "./data/traits.rds")
+  return(traits)
+}
+
+loadtraits2global <- function(){
+  traits <<- read.rds("./data/traits.rds")
+  return(traits)
+}
--- app.R
+++ app.R
@@ -35,15 +35,8 @@ library(tippy)
 model_data <- load_model_data()
 new_data_mean <- get_new_data_mean(model_data)
 
-traits <<- read.csv("../sflddata/private/data/raw/Australian_Bird_Data_Version_1.csv", 
-                   stringsAsFactors = FALSE) %>%
-  dplyr::filter(X3_Taxon_common_name_2 %in% model_data$species) %>%
-  dplyr::select(`Common Name` = X3_Taxon_common_name_2,
-                `Scientific Name` = X7_Taxon_scientific_name_CandB_2, 
-                `Body Length` = X96_Body_length_8,
-                `Body Mass` = X99_Body_mass_average_8) %>%
-  dplyr::mutate(`Body Length` = as.numeric(`Body Length`),
-                `Body Mass` = as.numeric(`Body Mass`)) 
+# preptraits(model_data)
+loadtraits2global()
 tempdir <- tempdir()
 report_path <- paste0(tempdir, "/", "report.Rmd") #file location assumes host is a unix machine
 stopifnot(file.copy("report.Rmd", report_path, overwrite = TRUE)) 
