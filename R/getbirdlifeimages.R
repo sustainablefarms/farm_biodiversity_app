1fc82ba28e7e3c21bf0720b0791bae402d59d5d7
Kassel Hingee <kassel.hingee@gmail.com>
2021-03-22 12:26:10 +1100

    code to download birdlife images

---
+++ R/getbirdlifeimages.R
@@ -0,0 +1,26 @@
+birdlife_getimages <- function(commonnames){
+  urls <- paste0("https://birdlife.org.au/bird-profile/", gsub(" ", "-", commonnames))
+  names(urls) <- commonnames
+  imgurls <- lapply(urls, function(url){
+    xmlofpage <- xml2::read_html(url)
+    imgpath <- xmlofpage %>%
+      rvest::html_nodes(".page-title") %>%
+      rvest::html_nodes("img") %>%
+      `[[`(1) %>%
+      rvest::html_attr("src")
+    imgurl <- paste0("https://birdlife.org.au", imgpath)
+    return(imgurl)
+  })
+  dir.create("./data/birdstories/", showWarnings = FALSE)
+  destfiles <- paste0("./data/birdstories/", gsub(" ", "-", names(imgurls)), ".", gsub(".*\\.", "", imgurls))
+  mapply(download.file, url = imgurls, destfile = destfiles)
+  return(destfiles)
+}
+
+prep_birdimages <- function(){
+  model_data <- load_model_data()
+  imgfilenames <- birdlife_getimages(model_data$species)
+  
+  saveRDS(imgfilenames, file = "./data/imgfilenames.rds")
+  return(imgfilenames)
+}
