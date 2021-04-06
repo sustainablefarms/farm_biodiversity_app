c420bbdfd346b63280eb2c30d725ab54c70d73c5
Kassel Hingee <kassel.hingee@gmail.com>
2021-04-06 11:28:02 +1000

    link new tab function created

---
+++ R/linknewtab.R
@@ -0,0 +1,8 @@
+# special tags$a that builds a hyperref that opens a link in new page:
+linknewtab <- function(...){
+  tags$a(
+    target = "_blank",
+    rel = "noreferrer noopener",
+    ...
+  )
+}
