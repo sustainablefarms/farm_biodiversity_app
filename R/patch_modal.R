f06f1561f7961c133c4f938f58dc750a38bbc176
Kassel Hingee <kassel.hingee@gmail.com>
2021-05-07 17:27:39 +1000

    converted noisy miners to a tooltip rather than a popover, but tooltips have some transparency

--- R/patch_modal.R
+++ R/patch_modal.R
@@ -24,14 +24,13 @@ patch_modal <- function(
         # 500m WCF
         tags$div(
           tags$html(tags$span("Woody vegetation canopy within 500m of patch centre (% area)"),
-                    infotooltip(title = paste("The amount of woody vegetation canopy within 500m of the patch centre,",
-                                                "as a percentage of the area within 500m.",
-                                              "Maps of woody vegetation canopy amounts can be explored at ",
+                    infotooltip(title = tags$div("This is the area of woody vegetation canopy, measured as a proportion of the total land area with 500m.",
+                                              "It changes each year as the trees react to the prevailing weather.",
+                                              "The percentage area of woody vegetation canopy can be obtained from",
                                               linknewtab(href = 'http://anuwald.science/tree',
                                                          "http://anuwald.science/tree"),
-                                              ".",
-                                              "The available values cover 90% of our training data."),
-                                placement = "auto bottom")
+                                              "for any Australian location and any year since 1990.",
+                                              HTML("<br><br>The values available for selection were chosen to cover 90% of our training data.")))
                     ),
           sliderInput(label = NULL,
             inputId = ns(paste0("pc_woody500m_", value)),
@@ -40,58 +39,33 @@ patch_modal <- function(
           ),
         tags$div(
           tags$html(tags$span("Woody vegetation canopy within 3km of patch centre (% area)"),
-                          infopopover(title = "Woody Vegetation Cover",
-                                      content = paste("The amount of woody vegetation canopy within 500m of the patch centre,",
-                                                      "as a percentage of the area within 500m.", "The available values cover 90% of our training data."))),
+                    infotooltip(title = tags$div("This is the area of woody vegetation canopy, measured as a proportion of the total land area with 500m.",
+                                              "It changes each year as the trees react to the prevailing weather.",
+                                              "The percentage area of woody vegetation canopy can be obtained from",
+                                              linknewtab(href = 'http://anuwald.science/tree',
+                                                         "http://anuwald.science/tree"),
+                                              "for any Australian location and any year since 1990.",
+                                              HTML("<br><br>The values available for selection were chosen to cover 90% of our training data.")),
+                                placement = "auto bottom")
+                    ),
           sliderInput(label = NULL,
             inputId = ns(paste0("pc_woody3000m_", value)),
             min = 2, max = 20, step = 2,
             value = woody3000m)
           ),
       tags$div(
-        class = "form-group shiny-input-container",
-        tags$span(
-          class = "checkbox",  #for some reason this creates an object that ends with a new line
-          style = 'display:inline',
-          tags$label(style='display:inline',tags$input(
-            id = ns(paste0("IsRemnant_", value)),
-            type = "checkbox",
-            checked = if (IsRemnant){TRUE} else {NULL},
-            tags$span("Is this patch remnant woodland?")
-          ))
+        inlinecheckBoxInput(ns(paste0("IsRemnant_", value)),
+            value = if (IsRemnant){TRUE} else {NULL},
+            label = tags$span("Is this patch remnant woodland?")
+          )
         ),
-        infopopover(title = "Remnant Woodland",
-                    content = 
-            tags$html(tags$div(
-              style="text-align: center",
-                linknewtab(href="https://birdlife.org.au/bird-profile/noisy-miner",
-                   tags$img(src="https://images.ala.org.au/image/proxyImageThumbnailLarge?imageId=37e23d13-9cd2-465f-ab87-9a79b641752a",
-                            alt="Noisy Miner Photo",
-                            width=200,
-                            height=200))),
-                 tags$div(
-                 "Noisy Miners noisily defend their ‘patch’ of trees from other birds. ...",
-                 #, especially other species of honeyeaters which may be seen as competitors for the food resources, and these are vigorously chased away.",
-                 # "Many other small birds are also driven from the area, and sometimes miners will even chase after cormorants or herons that may fly past, or harass them mercilessly if they perch somewhere in the miners’ territory.",
-                 "Because of this aggressive behaviour, areas inhabited by Noisy Miners often support few other birds.",
-                 "Image and text from",
-                 linknewtab(href="https://birdlife.org.au/bird-profile/noisy-miner",
-                        "BirdLife Australia"))
-                    ))),
+      tags$br(),
       tags$div(
-        class = "form-group shiny-input-container",
-        tags$span(
-          class = "checkbox",  #for some reason this creates an object that ends with a new line
-          style = 'display:inline',
-          tags$label(style='display:inline',tags$input(
-            id = ns(paste0("noisy_miner_", value)),
-            type = "checkbox",
-            checked = if (noisy_miner){TRUE} else {NULL},
-            tags$span("Noisy Miners present?")
-          ))
+        inlinecheckBoxInput(ns(paste0("noisy_miner_", value)),
+                            value = if (noisy_miner){TRUE} else {NULL},
+                            tags$span("Noisy Miners present?")
         ),
-        infopopover(title = "Noisy Miners",
-                    content = 
+        infotooltip(title = 
             tags$html(tags$div(
               style="text-align: center",
                 linknewtab(href="https://birdlife.org.au/bird-profile/noisy-miner",
@@ -107,7 +81,9 @@ patch_modal <- function(
                  "Image and text from",
                  linknewtab(href="https://birdlife.org.au/bird-profile/noisy-miner",
                         "BirdLife Australia"))
-                    ))),
+                    )),
+          `data-style` = "width = 100px;"
+        ),
       actionButton(inputId = ns("choose_patch_attributes_execute"), label = "Save"),
       modalButton("Cancel"),
     title = paste0("Select attributes for patch #", value),
