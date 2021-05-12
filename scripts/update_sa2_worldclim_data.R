76077289ebbfb1bf418e40a67ad2a84e9c94bace
Kassel Hingee <kassel.hingee@gmail.com>
2021-05-12 12:25:27 +1000

    saving sa2 worldclim data script

---
+++ scripts/update_sa2_worldclim_data.R
@@ -0,0 +1,23 @@
+sa2_points <- readRDS("../../farm_biodiversity_app/data/sa2_points.rds")
+sa2_points <- sa2_points[, c("longitude", "latitude", "label", "state", "color")]
+
+# Get worldclim data at locations
+# NOTE: This script will drop a file called wc0.5 in your home directory
+  # you can delete it when done
+
+locs <- sf::st_as_sf(sa2_points, coords = c("longitude", "latitude"), crs = 4326)
+result_df <- sflddata::ll2worldclim(locs)
+
+
+# cut down to only data that the model needs
+result_df <- result_df[, c(
+"AnnMeanTemp",
+"AnnPrec",
+"MaxTWarmMonth",
+"PrecWarmQ",
+"MinTColdMonth",  
+"PrecColdQ",
+"PrecSeasonality")]
+result_df <- cbind(sa2_points, result_df)
+
+saveRDS(result_df, "../../farm_biodiversity_app/data/sa2_points_climate.rds")
