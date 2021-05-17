0b6616f604f926c0d121d6cfeb083d37ab94e5f1
Kassel Hingee <kassel.hingee@gmail.com>
2021-05-17 13:59:49 +1000

    small polishes

--- R/patchdeschtml.R
+++ R/patchdeschtml.R
@@ -2,5 +2,5 @@ patchdefn <- tags$div(HTML("For our estimates a patch is a region of woodland ve
 HTML("(1) is 1ha - 10ha in area (approximately)<br>"),
 HTML("(2) has similar vegetation structure throughout, and <br>(3) is approximately 50m from other woody vegetation."),
 HTML("<br><br>"),
-HTML("The patch must box gum grassy woodland that is either be<br>(a) entirely planted vegetation or<br>(b) remnant.")
+HTML("The patch must be box gum grassy woodland that is either<br>(a) planted vegetation or<br>(b) remnant.")
 )
