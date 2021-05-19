b2623357fc90fbcd716dbae0c945af3cc107fb13
Kassel Hingee <kassel.hingee@gmail.com>
2021-05-19 19:00:43 +1000

    made copyright info smaller, so that species names easier to notice

--- R/speciesimgplots.R
+++ R/speciesimgplots.R
@@ -1,14 +1,9 @@
 # functions used in predictionsdetails_module for plotting species images
 
 specimageOut <- function(specinfo, height = "200px"){
-	if (grepl("birdlifephotography.org.au", specinfo$copyrightholder)){
-		cpyrht <- tags$div(linknewtab(href = "birdlifephotography.org.au",
+		cpyrht <- tags$div(style = "font-size: 70%", linknewtab(href = "birdlifephotography.org.au",
                                      HTML(paste0("&copy;",
 				     gsub("birdlifephotography.org.au", "", specinfo$copyrightholder)))))
-	} else {
-		cpyrht <- tags$div(HTML(paste0("&copy;", specinfo$copyrightholder))
-				   )
-	}
 
      out <- tags$div(specinfo$species,
 		 infotooltip(title = specinfo$story),
