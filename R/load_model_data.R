24b13c45e46cfdc687a6a3ff5e5b83fb0c514517
Kassel Hingee <kassel.hingee@gmail.com>
2021-05-24 14:21:21 +1000

    new data mean uses average WCF, not exp(average log WCF)

--- R/load_model_data.R
+++ R/load_model_data.R
@@ -10,8 +10,8 @@ get_new_data_mean <- function(model_data){
   colnames(new_data_mean) <- names(model_data$XoccProcess$center)
   # new_data_mean <- new_data_mean[, c(2:10)]
   new_data_mean$NMdetected[1] <- 1
-  new_data_mean$WCF_500 <- exp(new_data_mean$log.WCF_500.)
-  new_data_mean$WCF_3000 <- exp(new_data_mean$log.WCF_3000.)
+  new_data_mean$WCF_500 <-  9.48 #average percent - baked in as average log value is not log of average
+  new_data_mean$WCF_3000 <- 9.07
   new_data_mean$log.WCF_500. <- NULL
   new_data_mean$log.WCF_3000. <- NULL
   return(new_data_mean)
