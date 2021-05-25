c9f253f5b00b283f024f1655ff50cb5c474124cb
Kassel Hingee <kassel.hingee@gmail.com>
2021-05-26 07:51:16 +1000

    patch description updated

--- R/patchdeschtml.R
+++ R/patchdeschtml.R
@@ -1,6 +1,8 @@
-patchdefn <- tags$div(HTML("For our estimates a patch is a region of woodland vegetation that<br>"),
-HTML("(1) is 1ha - 10ha in area (approximately)<br>"),
-HTML("(2) has similar vegetation structure throughout, and <br>(3) is approximately 50m from other woody vegetation."),
-HTML("<br><br>"),
-HTML("The patch must be box gum grassy woodland that is either<br>(a) planted vegetation or<br>(b) remnant.")
+patchdefn <- tags$div(HTML("For our estimates a patch is an area of native woody vegetation that"),
+tags$ol(
+  tags$li("is 1ha - 10ha in area (approximately)"),
+  tags$li("is either remnant box gum grassy woodland, or a planting of native trees and/or shrubs"),
+  tags$li("has similar vegetation structure throughout, and"),
+  tags$li("is approximately 50m from other woody vegetation."),
+  )
 )
