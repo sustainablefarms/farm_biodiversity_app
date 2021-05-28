e86ede80daa8e716c904b05630540b0968e3aee0
Kassel Hingee <kassel.hingee@gmail.com>
2021-05-28 18:34:46 +1000

    improvements to WCF description

---
+++ R/WCFdesc.R
@@ -0,0 +1,9 @@
+WCFdesc <- function(){tags$p("The area of woody vegetation canopy changes over time due to water availability", 
+ linknewtab(href = "https://doi.org/10.1016/j.jag.2020.102209",
+	    "(Liao et al. IJAEOG, 2020)"),
+"and disturbances or management (such as fire or planting more trees).",
+                                              "The percentage area of woody vegetation canopy can be obtained from",
+                                              linknewtab(href = 'http://anuwald.science/tree',
+                                                         "http://anuwald.science/tree"),
+                                              "for any Australian location and any year since 1990.")}
+# used in functional form so that it can find 'linknewtab'
