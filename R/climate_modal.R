3ae30a22fcdcd56b04b3fdb053de6946a527a55b
Kassel Hingee <kassel.hingee@gmail.com>
2021-06-03 12:01:07 +1000

    climate modal disappears nicely now

--- R/climate_modal.R
+++ R/climate_modal.R
@@ -3,11 +3,10 @@ climate_modal <- function(ns, ...){
     modalDialog(
       plotOutput(ns("climate_plot")),
       ...,
-      tags$br(),
       "Each of the regions possible to select for this app is represented by a dot.",
       "The large dot is the value for your region,",
       "which is the worldclim estimate for the centre of your region.",
-      footer = NULL,
+      fade = TRUE,
       easyClose = TRUE
     )
   )
