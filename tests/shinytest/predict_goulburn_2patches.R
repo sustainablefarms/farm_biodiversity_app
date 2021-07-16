6d24e1d804ed8ce24914db19aa579657f99eb7fd
Kassel Hingee <kassel.hingee@gmail.com>
2021-07-16 12:41:36 +1000

    goulburn predicting for 2 patches, with one patch very small

--- tests/shinytest/predict_goulburn_2patches.R
+++ tests/shinytest/predict_goulburn_2patches.R
@@ -1,20 +1,31 @@
 app <- ShinyDriver$new("../../")
 app$snapshotInit("predict_goulburn_2patches")
 
-app$setInputs(`location-fake_region_number` = "click")
+app$setInputs(overallhelpfake = "click")
+app$setInputs(moredetailfake = "click")
+app$setInputs(downloadreportfake = "click")
+app$setInputs(introfake = "click")
+app$findElement('button[data-dismiss]')$click() #closes the modal
+
+# select goulburn
+# simulating a plotly click on goulburn below. To change to other locations I suspect changing the pointNumber will suffice
+#plotly_click is the event type, 'region_map' is the source. Combined with a "-" give the eventID in plotly::event_data
+# the event info I think can be obtained from session$rootScope()$input[[eventID]]
+# the value when clicking can be obtained by print(session$rootScope()$input[["plotly_click-region_map"]]) in a reactive in selectionlocation_module
+app$setInputs(`plotly_click-region_map` = '[{\"curveNumber\":0,\"pointNumber\":0,\"x\":149.603897167433,\"y\":-34.5156485191709}]', allowInputNoBinding_ = TRUE, priority_ = "input")
+
+# specify the patch number
 app$setInputs(`patch-choose_n_patches` = "click")
-app$setInputs(`patch-n_patches` = 2)
-app$waitForShiny()
-app$snapshot()
+app$setInputs(`patch-n_patches` = "2")
 app$setInputs(`patch-choose_n_patches_execute` = "click")
+# specify patch 1
 app$setInputs(`patch-patch_number_1` = "click")
-app$setInputs(`patch-noisy_miner_1` = TRUE, `patch-pc_woody_veg_1` = 14, `patch-pc_midstorey_1` = 8)
-app$waitForShiny()
-app$snapshot()
-app$waitForShiny()
 app$setInputs(`patch-choose_patch_attributes_execute` = "click")
+
+# specify patch 2
 app$setInputs(`patch-patch_number_2` = "click")
-app$setInputs(`patch-noisy_miner_2` = TRUE, `patch-pc_midstorey_2` = 10, `patch-pc_woody_veg_2` = 20)
+app$setInputs(`patch-pc_woody500m_2` = 2)
 app$setInputs(`patch-choose_patch_attributes_execute` = "click")
-app$waitForShiny()
+
+Sys.sleep(0.5)
 app$snapshot()
