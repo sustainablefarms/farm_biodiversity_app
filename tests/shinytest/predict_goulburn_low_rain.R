6a88b54c5642549189e4229c28568f38ed90fdc4
Kassel Hingee <kassel.hingee@gmail.com>
2021-07-16 12:48:46 +1000

    low rain prediction test recorded

---
+++ tests/shinytest/predict_goulburn_low_rain.R
@@ -0,0 +1,27 @@
+app <- ShinyDriver$new("../../")
+app$snapshotInit("predict_goulburn_low_rain")
+
+app$setInputs(overallhelpfake = "click")
+app$setInputs(moredetailfake = "click")
+app$setInputs(downloadreportfake = "click")
+app$setInputs(introfake = "click")
+app$findElement('button[data-dismiss]')$click() #closes the modal
+
+# select goulburn
+# simulating a plotly click on goulburn below. To change to other locations I suspect changing the pointNumber will suffice
+#plotly_click is the event type, 'region_map' is the source. Combined with a "-" give the eventID in plotly::event_data
+# the event info I think can be obtained from session$rootScope()$input[[eventID]]
+# the value when clicking can be obtained by print(session$rootScope()$input[["plotly_click-region_map"]]) in a reactive in selectionlocation_module
+app$setInputs(`plotly_click-region_map` = '[{\"curveNumber\":0,\"pointNumber\":0,\"x\":149.603897167433,\"y\":-34.5156485191709}]', allowInputNoBinding_ = TRUE, priority_ = "input")
+
+# specify the patch
+app$setInputs(`patch-patch_number_1` = "click")
+app$setInputs(`patch-pc_woody3000m_1` = 15.5)
+app$setInputs(`patch-pc_woody500m_1` = 14)
+app$setInputs(`patch-choose_patch_attributes_execute` = "click")
+
+# set rainfall
+app$setInputs(`yfa-AnnPrec.YfA` = 460)
+
+# capture predictions
+app$snapshot()
