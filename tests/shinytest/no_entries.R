ede365f8de1ad4a8fca1f26c47f0c8c76c5a075b
Kassel Hingee <kassel.hingee@gmail.com>
2021-07-16 12:52:15 +1000

    no entries test updated

--- tests/shinytest/no_entries.R
+++ tests/shinytest/no_entries.R
@@ -1,4 +1,10 @@
 app <- ShinyDriver$new("../../")
 app$snapshotInit("no_entries")
 
+app$setInputs(overallhelpfake = "click")
+app$setInputs(moredetailfake = "click")
+app$setInputs(downloadreportfake = "click")
+app$setInputs(introfake = "click")
+app$findElement('button[data-dismiss]')$click() #closes the modal
+
 app$snapshot()
