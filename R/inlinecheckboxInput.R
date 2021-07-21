482ee4cdc98619ff002f4714921d319413b58dc1
Kassel Hingee <kassel.hingee@gmail.com>
2021-07-21 20:11:03 +1000

    checkbox-inline class works visually but reactivity is off for some reason

--- R/inlinecheckboxInput.R
+++ R/inlinecheckboxInput.R
@@ -7,7 +7,8 @@ inlinecheckBoxInput <- function(inputId, label, value = FALSE, width = NULL){
   if (!is.null(value) && value) 
     inputTag$attribs$checked <- "checked"
   tags$span(class = "form-group shiny-input-container", style = htmltools::css(width = validateCssUnit(width)), 
-      tags$span(class = "checkbox",
+      tags$span(class = "checkbox", # class of checkbox-inline renders ok but reactivity is strange.
+                                    # This class was found in http://bootstrapdocs.com/v3.3.6/docs/css/#forms
            style = htmltools::css(display = "inline"), 
            tags$label(inputTag, tags$span(label))))
 }
