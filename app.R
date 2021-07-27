a76043266bf69ab4d795d5b59edd7830dd240db8
Kassel Hingee <kassel.hingee@gmail.com>
2021-07-28 08:34:38 +1000

    app.R needed some more blank lines at end of document to get shinytest running

--- app.R
+++ app.R
@@ -1,12 +1,7 @@
 # See README for more on running and deploying this app
 options(
   shiny.launch.browser = FALSE,
-  shiny.port = 7214,
-  shiny.testmode = FALSE
+  shiny.port = 7214
 )
 pkgload::load_all("."); myapp()
-# pkgload::load_all("."); app_selectpatchonly()
-# pkgload::load_all("."); app_selectlocationonly()
-# pkgload::load_all("."); app_predictionsonly()
-# pkgload::load_all("."); app_predictiondetailsonly()
-# pkgload::load_all("."); app_patchattronly()
+
