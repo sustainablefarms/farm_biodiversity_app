3e8559126647c68d5953b62b5f62db206d1c22d9
Kassel Hingee <kassel.hingee@gmail.com>
2021-08-17 14:19:35 +1000

    stopped other input ids in the selectpatch module from bookmarking

--- R/module_selectpatch.R
+++ R/module_selectpatch.R
@@ -203,7 +203,8 @@ selectpatchServer <- function(id){
   })
   
   
-  setBookmarkExclude(c(paste0("patch_number_", 1:maxpatchnum),
+  setBookmarkExclude(c("patch_selector",
+		       paste0("patch_number_", 1:maxpatchnum),
                        "choose_patch_attributes_execute"))
   
   # Save extra values in state$values when we bookmark
