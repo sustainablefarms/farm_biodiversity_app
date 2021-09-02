96dbea51ba16cfa348c2ef2d7d4f87524e6e74f3
Kassel Hingee <kassel.hingee@gmail.com>
2021-09-02 16:52:15 +1000

    species richness colors corrected

--- R/plot_richness_ggplot.R
+++ R/plot_richness_ggplot.R
@@ -1,13 +1,15 @@
 # richness plot
 
 richness_plot <- function(species_richness){
-  plot <- ggplot(species_richness, aes(x = category, y = E, fill = category)) +
+  plot <- ggplot(species_richness, aes(x = category, y = E, fill = E)) +
   geom_bar(stat = "identity") +
   scale_y_continuous(expand = c(0, 0)) +
   expand_limits(
     y = c(0, max(25, species_richness$E * 1.1))) +
   scale_x_discrete(position = "top") +
-  scale_discrete_manual(aesthetics = "fill", values = c("#81a2b3", "#4e839c", "#81a2b3", "#4e839c")) +
+  scale_fill_gradient(aesthetics = "fill",
+                      low = "#d0e7f4",
+                      high = "#178BCA") +
   # geom_errorbar(aes(ymin = E - 2 * sqrt(V), ymax = E + 2 * sqrt(V)), width = 0.2) +
   coord_flip() +
   ggtitle(NULL) +
@@ -16,11 +18,12 @@ richness_plot <- function(species_richness){
         axis.text.y = element_text(size = 12),
         axis.ticks.y = element_blank(),
         panel.grid.minor.x = element_line(color = "grey80"),
-        panel.grid.major.x = element_line(color = "grey80"),
+        panel.grid.major.x = element_line(color = "grey90"),
         panel.grid.minor.y = element_blank(),
         panel.grid.major.y = element_blank(),
-        panel.background = element_rect(fill = "grey90", colour = NA),
-        panel.border = element_blank()
+        panel.background = element_rect(fill = NA, colour = NA),
+        panel.border = element_rect(color = "grey90", fill = NA)
   )
+  # plot
   return(plot)
 }
