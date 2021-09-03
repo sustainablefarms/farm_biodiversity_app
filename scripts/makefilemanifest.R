3e0b7bbc7cb4b28d59e4d075588ddec9b3dc5499
Kassel Hingee <kassel.hingee@gmail.com>
2021-09-03 15:19:17 +1000

    deploying app with a file manifest seems to be much faster and more reliable - without it all the trash, all the high rest image and more would up bundled up and sent to shinyapps.io

---
+++ scripts/makefilemanifest.R
@@ -0,0 +1,27 @@
+rfiles <- list.files("./R", full.names = TRUE)
+appfile <- "app.R"
+
+wwwfiles1 <- list.files("./www", pattern = "^lowres.*", full.names = TRUE) # birdlife images
+wwwfiles2 <- paste0("./www/", c(
+  "Sustainable Farms logo RGB.png",
+  "google-analytics.html",
+  "base.css"))
+# setdiff(list.files("./www", full.names = TRUE), c(wwwfiles1, wwwfiles2))
+
+datafiles <- paste0("./data/", 
+c("nicecovarnames.csv",
+"report.Rmd",
+"alaimgs_final/c4c37912-34ea-420b-9c77-65b59a8c9391.jpg",
+"sa2_points_climate.rds",
+"state_borders.rds",
+"sa2_polygons.rds",
+"sa2_points.rds",
+"species_birdlifeimgs.csv",
+"species_shortstory.csv",
+"birdinfotable.rds",
+"consstatus.rds",
+"model_data.rds",
+"traits.rds"))
+
+files <- c(appfile, datafiles, rfiles, wwwfiles1, wwwfiles2)
+cat(files, file = "./data/filemanifest.txt", sep = "\n")
