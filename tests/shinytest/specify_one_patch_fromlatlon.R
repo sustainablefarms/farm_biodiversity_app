d2822065beff31adaa3ce2d74f2466944083b3f0
Kassel Hingee <kassel.hingee@gmail.com>
2021-10-01 17:08:43 +1000

    correction to test of from latlon

--- tests/shinytest/specify_one_patch_fromlatlon.R
+++ tests/shinytest/specify_one_patch_fromlatlon.R
@@ -1,4 +1,4 @@
-app <- ShinyDriver$new("../../", shinyOptions = list(test.mode = TRUE))
+app <- ShinyDriver$new("./", shinyOptions = list(test.mode = TRUE))
 app$snapshotInit("specify_one_patch_fromlatlon")
 
 app$setInputs(overallhelpfake = "click")
@@ -10,13 +10,13 @@ app$findElement('button[data-dismiss]')$click() #closes the modal
 app$setInputs(`patch-patch_number_1` = "click")
 Sys.sleep(1)
 app$snapshot()
-app$setInputs(`patch-patchattr-fromlatlon` = TRUE)
 app$setInputs(`patch-patchattr-getwoodycanopy` = "click") #the first default click of getwoodycanopy button
 Sys.sleep(1)
 app$snapshot()
 app$setInputs(`patch-patchattr-lon` = "145")
 app$setInputs(`patch-patchattr-lat` = "-35")
 app$setInputs(`patch-patchattr-yearforcanopy` = "2018")
+app$setInputs(`patch-patchattr-showmap` = FALSE)
 Sys.sleep(1)
 app$snapshot()
 app$setInputs(`patch-patchattr-getwoodycanopy` = "click") #retrieve woody canopy
