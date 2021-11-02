dd7f9eab669a4d8477d7569c89ee1461c9c37209
Kassel Hingee <kassel.hingee@gmail.com>
2021-11-02 15:47:53 +1100

    region plot seems to be working

--- R/plot_regions.R
+++ R/plot_regions.R
@@ -3,7 +3,8 @@ regionpts <- readRDS("data/sa2_points_climate.rds")
 regionpolygons <- readRDS("data/sa2_polygons.rds") %>%
   dplyr::filter(SA2_NAME16 %in% regionpts$label)
 # regionplot_dots(function(x)x)
-ns = function(x)x
+# ns = function(x)x
+# regionplot_borders(ns)
 
 regionplot_dots <- function(ns){
   out <- plotly::plot_ly(
@@ -51,44 +52,31 @@ dragmode = FALSE,
 
 
 regionplot_borders <- function(ns){
-  plot_ly() %>%
-    add_sf(data = regionpolygons,
-          type = "scatter",
-          mode = "lines",
-          color = ~SA2_NAME16,
-          fill = "toself",
-          hoveron = "fills"
-          # hoverinfo = 'text',
-          # text = ~SA2_NAME16
-          )
-    style(hoverinfo = TRUE,
-          hovertext = regionpolygons[ , "SA2_NAME16", drop = TRUE])
-  
-    add_sf(data = ,
-           hoverinfo = )
-          type = )
-  out <- plotly::plot_ly(
-    regionpts,
-    x = ~longitude,
-    y = ~latitude,
-    type = "scatter",
-    mode = "markers",
-    source = ns("region_map"),
-    marker = list(
-      size = 10,
-      color = ~color
-    ),
-    hoverinfo = "text",
-    text = ~label
-  ) %>%
-    add_sf(data = state_borders, 
-           type = "scatter", 
+  out <- plot_ly(source = ns("region_map")) %>% 
+    add_sf(data = state_borders,
+           type = "scatter",
            mode = "lines",
            inherit = FALSE,
            showlegend = FALSE,
            hoverinfo = 'none',
+           colors = RColorBrewer::brewer.pal(4, "Blues"), #needed this to stop warning with auto color
            line = list(color = "gray")
-    ) %>% 
+    ) %>%
+    add_sf(data = regionpolygons,
+          type = "scatter",
+          mode = "lines",
+          inherit = FALSE,
+          color = ~SA2_NAME16, #so that the SA2_NAME16 is registered as a data value
+          colors = RColorBrewer::brewer.pal(4, "Blues"), #needed this to stop warning with auto color
+          alpha = I(0),
+          stroke = I("black"),
+          text = ~SA2_NAME16,
+          hoverinfo = 'text',
+          hovertemplate = paste('%{text}<extra></extra>'),
+          # strokes = RColorBrewer::brewer.pal(4, "Blues"),
+          showlegend = FALSE
+         ) %>%
+
     add_text(x = min(regionpts$longitude)+1, 
              y = c(max(regionpts$latitude)-1, min(regionpts$latitude) - 0.5),
              text = c("NSW", "VIC"), 
