76eb1459bf282092af1c428617262371ddcc3d65
Kassel Hingee <kassel.hingee@gmail.com>
2021-11-03 22:52:20 +1100

    carousel controls are at the base of the carousel, but carousel size is changing based on contents

--- R/UI_carousel.R
+++ R/UI_carousel.R
@@ -5,6 +5,23 @@ carousel <- function(id,
   nslides <- length(slidecontents)
   stopifnot(nslides >= 2)
 tags$div(id = id, class = "carousel carousel-dark slide", `data-bs-ride`="carousel",
+  tags$div(class = "carousel-inner",
+           tags$div(class = "carousel-item active",
+                    slidecontents[[1]]),
+           lapply(2:nslides, function(idx){
+             tags$div(class = "carousel-item",
+                      slidecontents[[idx]])
+           })
+           ),
+  tags$div(style = "position:relative;", #footer, position relative to modify the absolute css positioning of the contents
+  tags$button(class = "carousel-control-prev", type = "button",
+              `data-bs-target`=paste0("#", id), `data-bs-slide` = "prev",
+              tags$span(class = "carousel-control-prev-icon", `aria-hidden`="true"),
+              tags$span(class = "visually-hidden", "Previous")),
+  tags$button(class = "carousel-control-next", type = "button",
+              `data-bs-target`=paste0("#", id), `data-bs-slide` = "next",
+              tags$span(class = "carousel-control-next-icon", `aria-hidden`="true"),
+              tags$span(class = "visually-hidden", "Next")),
   tags$div(class = "carousel-indicators",
     tags$button(type = "button", `data-bs-target` = paste0("#", id),
                 `data-bs-slide-to`="0", class = "active", `aria-current`="true",
@@ -15,22 +32,7 @@ tags$div(id = id, class = "carousel carousel-dark slide", `data-bs-ride`="carous
                   `aria-label`=paste("Slide", idx - 1))
     })
                 ),
-  tags$div(class = "carousel-inner",
-           tags$div(class = "carousel-item active",
-                    slidecontents[[1]]),
-           lapply(2:nslides, function(idx){
-             tags$div(class = "carousel-item",
-                      slidecontents[[idx]])
-           })
-           ),
-  tags$button(class = "carousel-control-prev", type = "button",
-              `data-bs-target`=paste0("#", id), `data-bs-slide` = "prev",
-              tags$span(class = "carousel-control-prev-icon", `aria-hidden`="true"),
-              tags$span(class = "visually-hidden", "Previous")),
-  tags$button(class = "carousel-control-next", type = "button",
-              `data-bs-target`=paste0("#", id), `data-bs-slide` = "next",
-              tags$span(class = "carousel-control-next-icon", `aria-hidden`="true"),
-              tags$span(class = "visually-hidden", "Next"))
+  )
 )}
 
 bird_gallery <- function(id, specinfotable){
