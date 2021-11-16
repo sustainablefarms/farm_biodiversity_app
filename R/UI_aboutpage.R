1b0c29653671a0038b91843e84f8e2aeee0ef813
Kassel Hingee <kassel.hingee@gmail.com>
2021-11-16 21:17:40 +1100

    got page rendering, deleted extra content

--- R/UI_aboutpage.R
+++ R/UI_aboutpage.R
@@ -44,9 +44,9 @@ aboutcontent <- function(){
     "The model accounts for detection difficulty and interspecies interaction",
     linknewtab(href = "https://doi.org/10.1002/ecy.2754",
 	       "[Tobler et al. 2019],"),
-    "although the latter is not included in", appname, "estimates due to the computational burden."
+    "although the latter is not included in", appname, "estimates due to the computational burden.",
     "We used expert bird surveys at 453 different patches of remnant Box Gum Grassy Woodland and 65 different patches of planted woodland to create this model.",
-    "These survey sites were located primarily in the NSW South Western Slopes bioregion, with a lesser number of surveys at location up to the Queensland-NSW border."
+    "These survey sites were located primarily in the NSW South Western Slopes bioregion, with a lesser number of surveys at location up to the Queensland-NSW border.",
     "The sites were surveyed in spring, spanning a timeframe of 17 years.",
     "A total of 5189 bird surveys were conducted."),
 
@@ -62,30 +62,8 @@ tags$p(appname, "was created using", linknewtab("??", "R"), linknewtab("??", "sh
        linknewtab("??", "R plotly"),
        linknewtab("??", "ggplot2"),
        linknewtab("??", "R Markdown")
-
-    "that",
-    "estimates occupancy of 62 different bird species in remnant or planted Box Gum Grassy Woodland patches.",
-    "The model was trained on bird surveys conducted in spring inside remnant Box Gum Grassy Woodland and planted woodland patches from 2000 to 2017.",
-    "Patches were in farmland in northern Victoria, NSW and southern QLD.",
-    "The model accounts for the detection difficulty of each species in these surveys, the windiness at the time of the survey, and time of day of the survey."
-  ),
-  tags$p("Bird species detected less in 100 or fewer surveys were not included due to a risk of overfitting the model."),
-
-  tags$p("Many environmental factors were tested for predicting bird occupancy.",
-         "The final model included the survey year,",
-         "summaries of long term climate and the previous 12 months,",
-         "the woody vegetation canopy within 500m and 3km of the patch center,",
-         "whether the patch was a remnant,",
-         "and whether noisy miners were detected at the patch that year."),
+       ),
   
-  tags$p("Setting the values of these factors for a farm has been simplified in the web app.",
-         "Survey year did not have a strong influence on bird occupancy, we suspect due to time-varying woody vegetation canopy, ",
-         "and is fixed to 2018.",
-         "Long term climate is given by the centre of the selected region.",
-         "The mean temperature of the previous 12 months was considered difficult for users to know, so was set to the long term average temperature.",
-         "The total precipitation of the previous 12 months did influence some bird occupancy according to our model, and can be set by users of the app.",
-         "The remaining climate-like summaries of the previous 12 months had little influence on bird occupancy and were set to mean of our data.",
-         "For each set of occupancy estimates, the full set of environmental values used to make the estimates are given in the report that can be downloaded."),
   tags$div(class='subheader', tags$h3("Further Information")),
   tags$p("A manuscript for scientific publication is under development.",
 	 "The source code for", appname, "is available at",
