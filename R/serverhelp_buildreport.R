8169a165fefd6eec8ad1f41ae82d52a9c515324a
Kassel Hingee <kassel.hingee@gmail.com>
2021-11-25 10:40:45 +1100

    building report added

---
+++ R/serverhelp_buildreport.R
@@ -0,0 +1,8 @@
+# function for building report
+buildreport <- function(cval, cpred, rval, rpred, refisaverage = TRUE, file){
+        rmarkdown::render(input = report_path, 
+                          params = list(loadexampledata = FALSE),
+                          output_file = file,
+                          envir = new.env(parent = environment())
+        )
+}
