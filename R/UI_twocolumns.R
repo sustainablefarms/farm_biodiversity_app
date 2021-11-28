7f4ac7123ef549273a912596760b2b632a342f26
Kassel Hingee <kassel.hingee@gmail.com>
2021-11-29 07:57:33 +1100

    changed twocolumns() to be 4 - 8 division as that is what is in Lauras design

--- R/UI_twocolumns.R
+++ R/UI_twocolumns.R
@@ -5,11 +5,11 @@ twocolumns <- function(
   ...
 ){
   fluidRow(
-    column(width = 3,
+    column(width = 4,
            if (!is.null(heading)){tags$h3(heading)}else{NULL},
            tags$div(class = "bodysmall", left)
     ),
-    column(width = 9,
+    column(width = 8,
            right
     ),
     class = "my-3",
