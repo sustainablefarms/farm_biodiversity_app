422910277a4093354cfafa8c930be50f6e6a4576
Kassel Hingee <kassel.hingee@gmail.com>
2022-03-01 14:47:31 +1100

    tests of get woody canopy for the year 2020 - this year is still strangely high

--- tests/testthat/test-getwoodycanopylatlon.R
+++ tests/testthat/test-getwoodycanopylatlon.R
@@ -4,3 +4,34 @@ test_that("WCF buffers can be fetched using latitude and longitude", {
   expect_equal(wcfs$`500m`, 0.72, tol = 0.01)
   expect_equal(wcfs$`3000m`, 1.04, tol = 0.01)
 })
+
+test_that("WCF buffers for 2020 are not strangely large", {
+  point <- sf::st_point(x = c(146.3333, -34.3333), dim = "XY")
+  pointwcrs <- sf::st_sf(sf::st_sfc(point, crs = 4326))
+  
+  within500m <- cloudget(pointwcrs, 500)
+  within3000m <- cloudget(pointwcrs, 3000)
+  within500m_baseline <- mean(within500m$WCF[within500m$Year != 2020])
+  within500m_baseline_sd <- sd(within500m$WCF[within500m$Year != 2020])
+  expect_lt(abs(within500m$WCF[within500m$Year == 2020] - within500m_baseline), 2 * within500m_baseline_sd)
+  
+  within3000m_baseline <- mean(within3000m$WCF[within3000m$Year != 2020])
+  within3000m_baseline_sd <- sd(within3000m$WCF[within3000m$Year != 2020])
+  expect_lt(abs(within3000m$WCF[within3000m$Year == 2020] - within3000m_baseline), 2 * within3000m_baseline_sd)
+})
+
+test_that("WCF buffers for 2020 are strangely large from THREDDS", {
+  point <- sf::st_point(x = c(146.3333, -34.3333), dim = "XY")
+  pointwcrs <- sf::st_sf(sf::st_sfc(point, crs = 4326))
+  
+  within3000m <- threddsget(pointwcrs, 3000, 1990:2020) %>%  #convert to same format as cloudget
+    tibble::as_tibble(rownames = "Year") %>%
+    tidyr::pivot_longer(everything(), 
+                        names_to = "Year", values_to = "WCF",
+                        names_prefix = "X"
+    ) %>%
+    dplyr::mutate(Year = as.integer(Year))
+  within3000m_baseline <- mean(within3000m$WCF[within3000m$Year != 2020])
+  within3000m_baseline_sd <- sd(within3000m$WCF[within3000m$Year != 2020])
+  expect_gt(abs(within3000m$WCF[within3000m$Year == 2020] - within3000m_baseline), 2 * within3000m_baseline_sd)
+  })
